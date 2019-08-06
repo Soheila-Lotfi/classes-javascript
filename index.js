b301e09bacdeed42063c8c53a63f920c70522bd6
soheila <soheilalotfi@ymail.com>
2019-08-06 18:48:26 -0400

    create a new instance of company

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // import our Company constructor
-var company = require("./company.js");
+var Company = require("./company.js");
 
 // create a new instance of Company
-var myCompany = new company(smartgardening, 10, true);
+var myCompany = new Company("smartgardening", 10, true);
 
 // add a 3 new employees to the instance of this company by calling the addEmployee method on your new company object
 
-addEmployees(mina, web - developer, 55000, true);
+myCompany.addEmployee("mina", "web - developer", 55000, true);
+myCompany.addEmployee("soli", "web - developer", 55000, true);
+myCompany.addEmployee("mh", "web - developer", 55000, false);
 
 // console.log your new object
+console.log(myCompany);
 
 // console.log the number of employees working your new company
+console.log(myCompany.numberOfEmployees);
